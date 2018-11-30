8965925608c6db0b5e7daea7d102e09050339ac0
Jonathan Arias <jagoraxr.moucha@gmail.com>
2018-11-29 23:13:27 -0600

    Update

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
 
 var url = window.location.href;
-var swLocation = 'twittor/sw.js';
+var swLocation = '/twittor/sw.js';
 
 if( navigator.serviceWorker ){
 
     if( url.includes('localhost') ){
-        swLocation = "/sw.js";
+        swLocation = '/sw.js';
     }
 
-    navigator.serviceWorker.register(swLocation);
+    navigator.serviceWorker.register(swLocation );
 }
 
 
